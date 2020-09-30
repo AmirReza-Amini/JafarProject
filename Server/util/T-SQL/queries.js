57945ed2f3efa84ed51ca2397bf9a8f2b8b49969
amirreza amini <amini.amirreza@sinathr.local>
2020-09-30 14:20:42 +0330

    queriesUpdated

--- Server/util/T-SQL/queries.js
+++ Server/util/T-SQL/queries.js
@@ -8,33 +8,15 @@ module.exports = {
             getVesselsList:LoadText(__dirname + '/basic-info/vessel/get-vessels-list.sql'),
             updateVessel:LoadText(__dirname + '/basic-info/vessel/update-vessel.sql'),
             insertVessel:LoadText(__dirname + '/basic-info/vessel/insert-vessel.sql')
-        },
-        SHIPPINGLINE:{
-            getShippingLinesList:LoadText(__dirname + '/basic-info/shippingLine/get-shippingLines-list.sql'),
-            updateShippingLine:LoadText(__dirname + '/basic-info/shippingLine/update-shippingLine.sql'),
-            insertShippingLine:LoadText(__dirname + '/basic-info/shippingLine/insert-shippingLine.sql')
-        }        
+        } ,
+        CURRENCY:{
+            loadLastCurrency:LoadText(__dirname + '/basic-info/currency/load-last-currency.sql')
+        }     
     },
     VOYAGE: {
         getVoyageList:LoadText(__dirname + '/basic-info/voyage/get-voyages-list.sql'),
         loadVoyageDwellById: LoadText(__dirname + '/voyage/load-voyage-dwell-by-id.sql'),
         loadVoyageDataById: LoadText(__dirname + '/voyage/load-voyage-data-by-id.sql')
-    },
-    OPERATOR: {
-        fetchOperatorInfoBasedOnCode: LoadText(__dirname + '/operator/fetch-operator-info-based-on-code.sql')
-    },
-    VESSEL: {
-        BERTH: {
-            getCntrInfoForUnload: LoadText(__dirname + '/vessel/berth/get-cntr-info-for-unload.sql'),
-            saveUnload: LoadText(__dirname + '/vessel/berth/save-unload.sql'),
-            addToShifting: LoadText(__dirname + '/vessel/berth/add-to-shifting.sql'),
-            addToLoadingList: LoadText(__dirname + '/vessel/berth/add-to-loading-list.sql'),
-            isExistCntrInInstructionLoading: LoadText(__dirname + '/vessel/berth/is-exist-cntr-in-instruction-loading.sql'),
-            saveUnloadIncrement: LoadText(__dirname + '/vessel/berth/save-unload-increment.sql')
-        },
-        DECK: {
-
-        }
     },
     BILLING: {
         GARBAGE_COLLECTION: {
