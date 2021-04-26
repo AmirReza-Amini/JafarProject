7a1f13187cd8c6f357d82e79f3a5c5c8c0b2bd60
uniqueboy-ara <ara.programmer@live.com>
2021-04-26 15:13:26 +0430

    TLS config

--- Server/server.js
+++ Server/server.js
@@ -2,37 +2,37 @@ require('express-async-errors')
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const serverHttps = require('https').createServer(app);
-const serverHttp = require('http').createServer(app);
-const setting = require('./app-setting')
+const { constants } = require('crypto')
+ const setting = require('./app-setting')
 const path = require('path')
-var fs = require('fs');
 
+var fs = require('fs');
 app.use(cors())
 app.use(express.json());
 app.use(express.static(__dirname + '/www'));
 app.use(require('./middleware/log'))
 app.use(require('./bootstrap/init'));
-var options = {
-  key: fs.readFileSync('./util/https/client-key.pem'),
-  cert: fs.readFileSync('./util/https/client-cert.pem')
-};
+
+const serverHttp = require('http').createServer(app);
+const serverHttps = require('https').createServer({
+  key: fs.readFileSync("./keys/client-key.pem"),
+  cert: fs.readFileSync("./keys/client-cert.pem"),
+    secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1,
+    passphrase: 'PASSWORD'
+  },app); 
+
+
+
 require('./bootstrap/mongodb');
 require('./routes')(app);
 
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/www/index.html'));
-// });
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
-
-//uncomment if you mind to have realtime actions
-//require('./messaging/socket')(app, server); 
 
 serverHttp.listen((setting.portNo), () => {
   console.log(`Http Server started on ${setting.portNo} --- ${new Date()}`);
 });
 
-serverHttps.listen((options,4100), () => {
+serverHttps.listen((4100), () => {
   console.log(`Https Server started on 4100 --- ${new Date()}`);
 });
